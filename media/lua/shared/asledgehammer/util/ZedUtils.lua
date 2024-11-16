df0c04e5e2b73d693c28f09ddaeb088442d6ba86
JabDoesThings <JabDoesThings@github.com>
2024-11-16 15:06:59 -0600

    Added 'ZedUtils.delay(ticks, callback)`.

--- media/lua/shared/asledgehammer/util/ZedUtils.lua
+++ media/lua/shared/asledgehammer/util/ZedUtils.lua
@@ -32,4 +32,40 @@ function ZedUtils.printLuaCommand(module, command, player, args)
     print(s);
 end
 
+--- Delays a task by x ticks.
+--- 
+--- @param ticks number The amount of ticks delayed before invoking the callback. <br>NOTE: If ticks is zero then the callback is invoked immediately.
+--- @param callback fun(): void The callback to invoke after the delay.
+--- 
+--- @return void
+function ZedUtils.delay(ticks, callback)
+    -- (Sanity checks)
+    assert(ticks > -1, 'Ticks cannot be negative. (Given: ' .. tostring(ticks) .. ')');
+    assert(callback ~= nil, 'The callback is nil!');
+    assert(type(callback) == 'function', 'The callback is not a function. (Given type: ' .. type(callback) .. ')');
+
+    -- Run immediatley.
+    if ticks == 0 then
+        callback();
+        return;
+    end
+
+    -- Run delayed.
+    local t = 0;
+    --- @type fun(): void | nil
+    local onTick = nil;
+    onTick = function()
+        if t < ticks then
+            t = t + 1;
+            return;
+        end
+        Events.OnFETick.Remove(onTick);
+        Events.OnTickEvenPaused.Remove(onTick);
+        callback();
+    end
+
+    Events.OnFETick.Add(onTick);
+    Events.OnTickEvenPaused.Add(onTick);
+end
+
 return readonly(ZedUtils);
