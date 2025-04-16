b96f2070482c47be2bf04f9f87bdb420506fce84
JabDoesThings <JabDoesThings@github.com>
2025-04-15 19:47:36 -0500

    Improved LuaNetworkEvents API.

--- media/lua/shared/asledgehammer/network/LuaNetworkEvents.lua
+++ media/lua/shared/asledgehammer/network/LuaNetworkEvents.lua
@@ -6,43 +6,115 @@
 --- @author JabDoesThings, asledgehammer 2025
 ---]]
 
+local readonly = require 'asledgehammer/util/readonly';
+
 -- (Network only)
 local IS_CLIENT = isClient();
 local IS_SERVER = isServer();
 if not IS_CLIENT and not IS_SERVER then return end
 
-LuaEventManager.AddEvent('OnLuaNetworkConnected');
 local triggered = false;
 local timeNow, timeLast = 0, 0;
 
+--- @generic E: any
+--- @param object any
+--- @param field string
+---
+--- @return E, nil
+local function getJavaField(object, field)
+    local offset = string.len(field);
+    for i = 0, getNumClassFields(object) - 1 do
+        local m = getClassField(object, i);
+        if string.sub(tostring(m), -offset) == field then
+            return getClassFieldVal(object, m);
+        end
+    end
+    return nil;
+end
+
+--- @param event string
+--- @param callback function
+local function AddFirst(event, callback)
+    --- callbacks: ArrayList<function>
+    local callbacks = getJavaField(LuaEventManager.AddEvent(event), 'callbacks');
+    if not callbacks then
+        print('Cannot Event.AddFirst(): Event doesn\'t exist: ' .. tostring(event));
+    end
+    callbacks:add(0, callback);
+end
+
+local module = {};
+local poolClient = {};
+local poolServer = {};
+
+--- @param func fun(module: string, command: string, player: IsoPlayer, args: any): void
+function module.addClientListener(func)
+    table.insert(poolClient, func);
+end
+
+--- @param func fun(module: string, command: string, args: any): void
+function module.addServerListener(func)
+    table.insert(poolServer, func);
+end
+
+Events.OnGameBoot.Add(function()
+    if IS_CLIENT then
+        AddFirst('OnServerCommand', function(m, command, args)
+            for _, callback in ipairs(poolServer) do
+                pcall(function() callback(m, command, args) end);
+            end
+        end);
+    elseif IS_SERVER then
+        AddFirst('OnClientCommand', function(m, command, player, args)
+            for _, callback in ipairs(poolClient) do
+                pcall(function() callback(m, command, player, args) end);
+            end
+        end);
+    end
+end);
+
+LuaEventManager.AddEvent('OnLuaNetworkConnected');
+
 if IS_CLIENT then
     Events.OnGameStart.Add(function()
-        local a, b;
-        a = function()
+        local a = function()
             timeNow = getTimeInMillis();
             if timeNow - timeLast > 500 then
                 sendClientCommand('asledgehammer_utilities', 'ping', {});
                 timeLast = getTimeInMillis();
             end
         end
-        b = function()
+        module.addServerListener(function(m, command, args)
+            if m ~= 'asledgehammer_utilities' then return end
+            if command ~= 'pong' then return end
             if not triggered then
                 triggered = true;
-                -- Confirm that this works.
                 triggerEvent('OnLuaNetworkConnected');
             end
-            Events.OnServerCommand.Remove(b);
             Events.OnTickEvenPaused.Remove(a);
-        end
+        end);
         Events.OnTickEvenPaused.Add(a);
-        Events.OnServerCommand.Add(b);
     end);
 elseif IS_SERVER then
     Events.OnServerStarted.Add(function()
-        Events.OnClientCommand.Add(function(module, command, player, args)
-            if module ~= 'asledgehammer_utilities' then return end
+        module.addClientListener(function(m, command, player)
+            if m ~= 'asledgehammer_utilities' then return end
             if command ~= 'ping' then return end
+            triggerEvent('OnLuaNetworkConnected', player);
             sendServerCommand(player, 'asledgehammer_utilities', 'pong', {});
         end);
     end);
 end
+
+Events.OnLuaNetworkConnected.Add(
+--- @param player? IsoPlayer
+    function(player)
+        if player then
+            print('### LUA NETWORK CONNECTION (Player "' .. player:getUsername() .. '") ###');
+        else
+            print('### LUA NETWORK CONNECTION ###');
+        end
+    end
+);
+
+return readonly(module);
